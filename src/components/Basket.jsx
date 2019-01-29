c03980aab28db637cfd1b76797ec1ff9f93e362f
Lilli <lillibaaske@gmx.de>
2019-01-29 23:45:15 +0100

    Initial commit

---
+++ src/components/Basket.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Basket extends Component {
+  render() {
+    return (
+      <div>
+          <h1>Hello Ribo and LIlli!</h1>
+      </div>
+    )
+  }
+}
